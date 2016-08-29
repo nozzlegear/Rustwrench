51685c6c3adf80f9ad3474dabeaeb171ac29cad2
Joshua Harms <joshua@nozzlegear.com>
2016-08-29 10:52:14 -0500

    Add Shopify OAuth props

--- Rustwrench/Models/User.cs
+++ Rustwrench/Models/User.cs
@@ -6,5 +6,11 @@ namespace Rustwrench.Models
         /// The user's username/id. MyCouch will automatically set this as the CouchDB id.
         /// </summary>
         public string UserId { get; set; }
+
+        public string ShopifyAccessToken { get; set; }
+
+        public string ShopifyUrl { get; set; }
+
+        public string ShopName { get; set; }
     }
 }
