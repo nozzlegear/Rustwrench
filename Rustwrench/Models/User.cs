d319c48a08c25720a3bbf69faaa37e61e73ef204
Joshua Harms <joshua@nozzlegear.com>
2016-08-29 13:19:36 -0500

    Add HashedPassword property

--- Rustwrench/Models/User.cs
+++ Rustwrench/Models/User.cs
@@ -7,10 +7,17 @@ namespace Rustwrench.Models
         /// </summary>
         public string UserId { get; set; }
 
+        /// <summary>
+        /// The user's BCrypt-hashed password.
+        /// </summary>
+        public string HashedPassword { get; set; }
+
         public string ShopifyAccessToken { get; set; }
 
         public string ShopifyUrl { get; set; }
 
         public string ShopName { get; set; }
+
+        public long? ShopId { get; set; }
     }
 }
